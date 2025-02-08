565ade3c95fb1d40a3971b0f4945ffbbf379abf5
Patrícia <patriciaferrantunes@gmail.com>
2025-02-08 23:26:14 +0000

    correct image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Jersey_10 } from "next/font/google";
 
@@ -9,16 +8,6 @@ const jersey = Jersey_10({
   weight: "400",
   display: 'swap',
 })
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Pixel2Design",
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center w-screen h-screen bg-[#2A1E5C]">
-      <img src="logo.png" alt="Pixel2Design Logo" />
+      <Image
+      src="/logo.png"
+      width={416}
+      height={229}
+      alt="Picture of the author"
+    />
       <p className="mt-10 text-white text-7xl text-center font-[family-name:var(--font-jersey10)]">a alinhar os pixeis ...</p>
     </div>
   );
