3de0c7de3d1579bdcb72ec663676480835f5934d
Patrícia <patriciaferrantunes@gmail.com>
2025-02-09 11:53:29 +0000

    commit contact form

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
-import Image from "next/image";
+import ContactForm from "../components/ContactForm"
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center w-screen h-screen bg-[#2A1E5C]">
-      <Image
-      src="/logo.png"
-      width={416}
-      height={229}
-      alt="Picture of the author"
-    />
-      <p className="mt-10 text-white text-7xl text-center font-[family-name:var(--font-jersey10)]">Teste ... a alinhar os pixeis ...</p>
+    <div className="">
+      <ContactForm />
     </div>
   );
 }
