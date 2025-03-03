f475039e822f3780274f6833bd48521cff18b30d
Patrícia <patriciaferrantunes@gmail.com>
2025-03-03 13:58:53 +0000

    add robots txt and site map

---
+++ app/sitemap.ts
@@ -0,0 +1,46 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://pixel2design.pt',
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          pt: 'https://pixel2design.pt/pt',
+          en: 'https://pixel2design.pt/en',
+        },
+      },
+    },
+    {
+      url: 'https://pixel2design.pt/services',
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          pt: 'https://acme.com/pt/services',
+          en: 'https://acme.com/en/services',
+        },
+      },
+    },
+    {
+      url: 'https://pixel2design.pt/terms',
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          pt: 'https://pixel2design.pt/pt/terms',
+          en: 'https://pixel2design.pt/en/terms',
+        },
+      },
+    },
+    {
+      url: 'https://pixel2design.pt/privacy',
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          pt: 'https://pixel2design.pt/pt/privacy',
+          en: 'https://pixel2design.pt/en/privacy',
+        },
+      },
+    },
+  ]
+}
