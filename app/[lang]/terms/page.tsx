c085443906bd1db2bd24a4b453030108e492fa9a
Patrícia <patriciaferrantunes@gmail.com>
2025-04-01 21:46:02 +0100

    metadata

--- app/[lang]/terms/page.tsx
+++ app/[lang]/terms/page.tsx
@@ -3,6 +3,35 @@ import { PortableText } from '@portabletext/react';
 import { SystemPages } from '@/types/system'
 import { richText } from '@/components/richText'
 import { Lang } from '@/types/lang'
+import { getMeta } from "@/lib/sanity/queries/meta";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ lang: Lang }> ;
+}) {
+  const { lang } = await params; // 'params' já resolvido aqui
+
+  const metadata = await getMeta(lang, "terms");
+
+  return {
+    metadataBase: new URL('https://pixel2design.pt/pt'),
+    alternates: {
+      canonical: '/',
+      languages: {
+        'pt-PT': '/pt',
+        'en-EN': '/en',
+      },
+    },
+    title: metadata?.title || "Pixel2Design",
+    description: metadata?.metadescription || "Pixel2Design",
+    openGraph: {
+      title: metadata?.metatitle || "Pixel2Design",
+      description: metadata?.metadescription || "Pixel2Design",
+      images: metadata?.ogImage ? [metadata.ogImage] : [],
+    },
+  };
+}
 
 
 export default async function Terms({
